fc3565aefbd80d7ecc68612d726872c6005033f8
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 15:35:00 -0300

    feat: rota POST para tabela employees

--- app/backend/src/routes/employee.ts
+++ app/backend/src/routes/employee.ts
@@ -5,5 +5,6 @@ const route = Router();
 const employeeController = new EmployeeController();
 
 route.get('/employees', (req, res) => employeeController.getAll(req, res));
+route.post('/employees', (req, res) => employeeController.create(req, res));
 
 export default route;
