1a4ef1deb7be4b9f27b24b499323d874c73266b9
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 17:54:27 -0300

    feat: rotas POST, PUT e DELETE na tabela de Department

--- app/backend/src/routes/department.ts
+++ app/backend/src/routes/department.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 import DepartmentController from '../controllers/Department';
+import departmentValidation from '../middlewares/departmentValidation';
 
 const route = Router();
 const departmentController = new DepartmentController();
 
 route.get('/departments', (req, res) => departmentController.getAll(req, res));
+route.post(
+  '/departments',
+  departmentValidation,
+  (req, res) => departmentController.create(req, res)
+);
+route.put(
+  '/departments/:id',
+  departmentValidation,
+  (req, res) => departmentController.update(req, res)
+);
+route.delete('/departments/:id', (req, res) => departmentController.delete(req, res));
 
 export default route;
