0534834a2c911e7690434dfa93e7fd80f8385a4e
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 17:52:48 -0300

    feat: métodos create, update e delete em DepartmentService

--- app/backend/src/services/Department.ts
+++ app/backend/src/services/Department.ts
@@ -1,4 +1,5 @@
 import Department from '../database/models/Department';
+import IUpdateDepartment, { IDepartment } from '../interfaces/IDepartment';
 
 export default class DepartmentService {
   private _departmentModel = Department;
@@ -6,5 +7,19 @@ export default class DepartmentService {
   public async getAll() {
     const departments = await this._departmentModel.findAll();
     return departments
+  }
+
+  public async create(info: IDepartment) {
+    await this._departmentModel.create({ ...info });
+    return { message: 'Departamento Cadastrado com Sucesso' };
+  }
+
+  public async update(info: IUpdateDepartment) {
+    await this._departmentModel.update({ ...info }, { where: { id: info.id } })
+    return { message: 'Departamento atualizado com Sucesso' };
+  }
+
+  public async delete(id: number) {
+    return await this._departmentModel.destroy({ where: { id } })
   }
 }
