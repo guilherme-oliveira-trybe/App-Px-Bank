021a6fe50d41f625ab27feed20ab42e3c0d4d71b
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 16:50:45 -0300

    feat: adicionando middleware de erro e ativando o Cors no app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import errorHandler from './middlewares/errors';
 import { departmentRoute, employeeRoute } from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/health', (_req, res) => res.status(200).send());
 
 app.use(departmentRoute);
 app.use(employeeRoute);
+app.use(errorHandler);
 
 export default app;
