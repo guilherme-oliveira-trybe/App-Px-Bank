998047acb2ccca50eb62a4f05d7b8d0be0eed2f6
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 12:10:41 -0300

    feat: adição da rota da tabela de Departments ao app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import departmentRoute from './routes/department';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/health', (_req, res) => res.status(200).send());
+
+app.use(departmentRoute);
 
 export default app;
