29d603125f678013fda26387d1069ce74503f810
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 15:34:22 -0300

    feat: adiciona método create a class EmployeeService

--- app/backend/src/services/Employee.ts
+++ app/backend/src/services/Employee.ts
@@ -1,10 +1,24 @@
 import Employee from '../database/models/Employee';
+import Department from '../database/models/Department';
+import { ICreate } from '../interfaces/IEmployee';
 
 export default class EmployeeService {
   private _employeeModel = Employee;
 
   public async getAll() {
-    const employees = await this._employeeModel.findAll();
+    const employees = await this._employeeModel.findAll({
+      include: {
+        model: Department,
+        as: 'departmentName',
+        attributes: ['department']
+      },
+      attributes: {exclude: ['departmentId']}
+    });
     return employees
+  }
+
+  public async create(info: ICreate) {
+    await this._employeeModel.create({...info});
+    return { message: 'Usuário Cadastrado com Sucesso'}
   }
 }
