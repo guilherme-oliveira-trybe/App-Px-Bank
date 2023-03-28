8d37eaca9db41c00e187f476b29e6c248eb4f129
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 10:39:09 -0300

    feat: migration de criação da tabela de funcionários

---
+++ app/backend/src/database/migrations/02-create-employees.js
@@ -0,0 +1,42 @@
+/* eslint-disable max-lines-per-function */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Employees', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cpf: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      salary: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      dateOfBith: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      departmentId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreigKey: true,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Departments',
+          key: 'id'
+        }
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Employees');
+  },
+};
