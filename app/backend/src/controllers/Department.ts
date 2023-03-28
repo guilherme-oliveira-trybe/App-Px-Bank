3b93a956eb242d1a9583626cbe591bb36c5a6d0a
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 17:53:02 -0300

    feat: métodos create, update e delete em DepartmentController

--- app/backend/src/controllers/Department.ts
+++ app/backend/src/controllers/Department.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { IDepartment } from '../interfaces/IDepartment';
 import DepartmentService from '../services/Department';
 
 export default class DepartmentController {
@@ -8,5 +9,25 @@ export default class DepartmentController {
     const result = await this._departmentService.getAll();
 
     return res.status(200).json(result);
+  }
+
+  public async create(req: Request, res: Response) {
+    const result = await this._departmentService.create(req.body as IDepartment);
+
+    return res.status(201).json(result);
+  }
+
+  public async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const department = {...req.body, id: Number(id)}
+    const result = await this._departmentService.update(department);
+
+    return res.status(200).json(result);
+  }
+
+  public async delete(req: Request, res: Response) {
+    await this._departmentService.delete(Number(req.params.id));
+
+    return res.status(204).end();
   }
 }
