aa3d006d22bf11142c519a2db29d0a2754f3f968
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 15:34:34 -0300

    feat: adiciona método create a class EmployeeController

--- app/backend/src/controllers/Employee.ts
+++ app/backend/src/controllers/Employee.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
 import EmployeeService from '../services/Employee';
+import { ICreate } from '../interfaces/IEmployee'
 
 export default class EmployeeController {
   private _employeeService = new EmployeeService();
 
-  public async getAll(req: Request, res: Response) {
+  public async getAll(_req: Request, res: Response) {
     const result = await this._employeeService.getAll();
+
+    return res.status(200).json(result);
+  }
+
+  public async create(req: Request, res: Response) {
+    const result = await this._employeeService.create(req.body as ICreate);
 
     return res.status(200).json(result);
   }
