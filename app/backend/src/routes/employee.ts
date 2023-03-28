92b6e07df5962d64646bc8c6ac266df4d89b7f96
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-28 17:31:36 -0300

    feat: adiciona o middleware de validação as rota de POST e PUT

--- app/backend/src/routes/employee.ts
+++ app/backend/src/routes/employee.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import EmployeeController from '../controllers/Employee';
+import employeeValidation from '../middlewares/employeeValidation';
 
 const route = Router();
 const employeeController = new EmployeeController();
 
 route.get('/employees', (req, res) => employeeController.getAll(req, res));
-route.post('/employees', (req, res) => employeeController.create(req, res));
-route.put('/employees/:id', (req, res) => employeeController.update(req, res));
+route.post('/employees', employeeValidation, (req, res) => employeeController.create(req, res));
+route.put('/employees/:id', employeeValidation, (req, res) => employeeController.update(req, res));
 route.delete('/employees/:id', (req, res) => employeeController.delete(req, res));
 
 export default route;
