b46fe5433ed459953237262d023fec1cd6cc2b1b
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-29 15:19:45 -0300

    feat: useEffect para chamada na api

--- app/frontend/src/pages/ListEmployees.js
+++ app/frontend/src/pages/ListEmployees.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Button from '../components/Button';
 import FilterEmployee from '../components/FilterEmployee';
 import TableEmployees from '../components/TableEmployees';
+import Context from '../context/Context';
+import { getAllDepartments } from '../services/api';
 
 function ListEmployees() {
+  const { setAllDepartment } = useContext(Context);
+
+  useEffect(() => {
+    const getDepartments = async () => {
+      const data = await getAllDepartments();
+      setAllDepartment(data);
+    };
+    getDepartments();
+  }, [setAllDepartment]);
+
   return (
     <section>
       <FilterEmployee />
