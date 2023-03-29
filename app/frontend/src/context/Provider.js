7c07051832080613ceff4f7dffe8f8285cfe559b
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-29 15:18:52 -0300

    feat: configura Providers para fornecer as informações buscadas na api de forma global

--- app/frontend/src/context/Provider.js
+++ app/frontend/src/context/Provider.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Context from './Context';
 
 // eslint-disable-next-line react/prop-types
 function Provider({ children }) {
+  const [allDepartment, setAllDepartment] = useState([]);
+
+  const value = useMemo(() => ({
+    allDepartment,
+    setAllDepartment,
+  }), [allDepartment]);
+
   return (
-    <Context.Provider>
+    <Context.Provider value={ value }>
       {children}
     </Context.Provider>
   );
