fb6c036fae84e33e516f48cb3beb1e656684c54f
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-29 15:37:49 -0300

    feat: adiciona ao Provider as informações buscada na api referente a todos os funciónarios

--- app/frontend/src/context/Provider.js
+++ app/frontend/src/context/Provider.js
@@ -4,11 +4,14 @@ import Context from './Context';
 // eslint-disable-next-line react/prop-types
 function Provider({ children }) {
   const [allDepartment, setAllDepartment] = useState([]);
+  const [allEmployees, setAllEmployees] = useState([]);
 
   const value = useMemo(() => ({
     allDepartment,
     setAllDepartment,
-  }), [allDepartment]);
+    allEmployees,
+    setAllEmployees,
+  }), [allDepartment, allEmployees]);
 
   return (
     <Context.Provider value={ value }>
