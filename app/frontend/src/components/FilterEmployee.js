f7d9e3a65de7b986916ec929410e6bcd0641446b
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-29 22:01:52 -0300

    refactor: adiciona preventDefault ao onSubmit do form

--- app/frontend/src/components/FilterEmployee.js
+++ app/frontend/src/components/FilterEmployee.js
@@ -3,12 +3,16 @@ import Context from '../context/Context';
 import Button from './Button';
 
 function FilterEmployee() {
-  const { allDepartment } = useContext(Context);
+  const { allDepartment, filters } = useContext(Context);
   const [name, setName] = useState('');
   const [filterDepartment, setFilterDepartment] = useState('');
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <label>
         Nome:
         <input
@@ -23,13 +27,13 @@ function FilterEmployee() {
           value={ filterDepartment }
           onChange={ ({ target }) => setFilterDepartment(target.value) }
         >
-          <option value="" disabled selected>Selecione o Departamento</option>
+          <option value="">Selecione o Departamento</option>
           {allDepartment.map(({ department, id }) => (
             <option key={ id }>{department}</option>
           ))}
         </select>
       </label>
-      <Button type="submit">
+      <Button type="submit" onClick={ () => filters(name, filterDepartment) }>
         Pesquisa
       </Button>
     </form>
