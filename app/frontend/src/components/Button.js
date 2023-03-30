ebc24e9eb18ff152bd6509438dadea566def66c0
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-30 20:37:43 -0300

    refactor: adiciona atributo disabled ao component Button

--- app/frontend/src/components/Button.js
+++ app/frontend/src/components/Button.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 // eslint-disable-next-line react/prop-types
-function Button({ children, type, onClick }) {
+function Button({ children, type, onClick, disabled = false }) {
   return (
     <button
       type={ type }
       onClick={ onClick }
+      disabled={ disabled }
     >
       {children}
     </button>
