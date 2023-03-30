afffcc8b94ea06bc98ffea24e32f70af98886326
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-30 12:46:55 -0300

    retira o Provider do App e passa para o Index

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Provider from './context/Provider';
 import ListEmployees from './pages/ListEmployees';
 
 function App() {
   return (
-    <Provider>
-      <main className="App">
-        <ListEmployees />
-      </main>
-    </Provider>
+    <main className="App">
+      <ListEmployees />
+    </main>
   );
 }
 
