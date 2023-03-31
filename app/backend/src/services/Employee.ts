2ab711401dd6e40ff71c9f419d72d94ddca9b161
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-03-31 10:45:48 -0300

    feat: Adiciona verificação de cpf já existente

--- app/backend/src/services/Employee.ts
+++ app/backend/src/services/Employee.ts
@@ -1,6 +1,7 @@
 import Employee from '../database/models/Employee';
 import Department from '../database/models/Department';
 import IUpdate, { ICreate } from '../interfaces/IEmployee';
+import CustomError from '../errors/CustomError';
 
 export default class EmployeeService {
   private _employeeModel = Employee;
@@ -19,13 +20,21 @@ export default class EmployeeService {
   }
 
   public async create(info: ICreate) {
+    const employeeFound = await this._employeeModel.findOne({ where: { cpf: info.cpf}});
+    if (employeeFound) {
+      throw new CustomError(400, 'CPF já cadastrado anteriormente')
+    }
     await this._employeeModel.create({...info});
     return { message: 'Funcionário Cadastrado com Sucesso' };
   }
 
   public async update(info: IUpdate) {
+    const employeeFound = await this._employeeModel.findOne({ where: { cpf: info.cpf}});
+    if (employeeFound && employeeFound.id !== info.id) {
+      throw new CustomError(400, 'CPF já cadastrado anteriormente')
+    }
     await this._employeeModel.update({ ...info }, { where: { id: info.id } })
-    return { message: 'Cadastro atualizado com Sucesso' };
+    return { message: 'Cadastro Atualizado com Sucesso' };
   }
 
   public async delete(id: number) {
